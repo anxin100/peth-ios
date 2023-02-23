86b5135ee7970aa76d7337c10d43d87063732905
qiangl <qiangl@opera.com>
2023-02-23 14:32:24 +0800

    Init project

---
+++ Peth/Peth/PethApp.swift
@@ -0,0 +1,17 @@
+//
+//  PethApp.swift
+//  Peth
+//
+//  Created by qiangl on 23.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct PethApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
